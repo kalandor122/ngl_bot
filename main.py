b2d130ebd32d37a8586ee78a7c326c47d305cd56
kalandor122 <47007141+kalandor122@users.noreply.github.com>
2023-01-04 13:27:49 +0100

    optimized and translated to English

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 link = input("link:")
-be = input("mit:")
-azanyi = int(input("hányszor:"))
+be = input("message:")
+azanyi = int(input("how many messages:"))
 
 driver = webdriver.Chrome()
 driver.get(link)
@@ -18,17 +18,7 @@ for i in range(azanyi):
         time.sleep(1)
         butt = driver.find_element("xpath", "/html/body/div[2]/a[2]").click()
         time.sleep(2)
-        if i / 5 == True:
-            driver.close()
-            driver = webdriver.Chrome()
-            driver.get(link)
-
-            elem = driver.find_element("name" ,"question")
-            elem.send_keys(be)
-            but = driver.find_element("xpath", "/html/body/div[1]/form/button").click()
-            time.sleep(1)
-            butt = driver.find_element("xpath", "/html/body/div[2]/a[2]").click()
-            time.sleep(2)
+        
     except:
         driver.close()
         driver = webdriver.Chrome()
